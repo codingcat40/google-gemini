06356deec5656cd3ec881d3b7992572feb3318a7
Naman Matoliya <naman@fieldproxy.com>
2025-09-21 14:42:19 +0530

    spinner, UI fixes, gemini Response

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 import { GoogleGenAI } from "@google/genai";
+import ReactMarkdown from 'react-markdown'
+import Loading from "./Loading";
 
 const Home = () => {
   const [prompt, setPrompt] = useState<string>("");
-  const [history, setHistory] = useState<Array<string>>([]);
-  const [response, setResponse] = useState<string | undefined>("");
+  const [responseHistory, setResponseHistory] = useState<Array<string | undefined>>([]);
+  const [history, setHistory] = useState<Array<string | undefined>>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
 
   const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
+    setLoading(true)
     const key = import.meta.env.VITE_API_KEY;
     const ai = new GoogleGenAI({ apiKey: key });
 
@@ -17,26 +22,47 @@ const Home = () => {
     });
 
     console.log(response.text);
-    setResponse(response.text)
+    setHistory(prev => [...prev, prompt])
+    setResponseHistory(prev => [...prev, response.text])
+    setLoading(false)
   };
 
 
-//   to fetch the past items
-  const fetchHistory = () => {
-    
-  }
+
 
   return (
     <div className="flex h-screen">
-      <div className="hidden md:block w-64 bg-red-200 border-r p-4">
-        Request History
+      <div className="hidden md:block flex-col w-64 border-r p-4">
+        <span>Request History</span>
+        <ul className="flex flex-col space-y-8 bg-gray-200 h-full overflow-y-auto">
+          {
+          history.length > 0 ? 
+            history.map((item, index) => {
+              return <li className="italic" key={index}>{item}</li>
+            })
+            : <span>Nothing to show here</span>  
+        }
+        </ul>
+
       </div>
 
       {/* Main Content */}
       <div className="flex-1 bg-white p-4 md:p-6 flex flex-col">
         {/* Chat box */}
-        <div className="flex-1 rounded-2xl border min-h-[400px] md:min-h-[648px] overflow-y-auto">
-          {/* messages/content here */}
+        <div className="flex-1 rounded-2xl border min-h-[400px] md:min-h-[648px] flex text-center justify-center overflow-y-auto">
+          {
+            loading ? <Loading /> :
+            responseHistory.length > 0 ? 
+            <div className="flex flex-col gap-12 overflow-y-auto w-full">
+                {
+                  responseHistory.map((item, index) => {
+                    return <ReactMarkdown key={index}>
+                        {item}
+                    </ReactMarkdown>
+                  })
+                }
+            </div> : <span className="mt-24 text-xl">Welcome to gemini 2.5, How can I help you today :)</span>
+          }
         </div>
 
         {/* Input area */}
